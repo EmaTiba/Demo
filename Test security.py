30bee6ac10156778e3218fe801b56cc4e794aaa6
Emanuele Tibaldi <113374209+EmaTiba@users.noreply.github.com>
2022-10-28 13:59:32 +0200

    Create Test security.py

---
+++ Test security.py
@@ -0,0 +1,22 @@
+
+while True print('Hello world')
+  File "<stdin>", line 1
+    while True print('Hello world')
+    
+    
+    import yaml
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f'<Person: {self.name} - {self.age}>'
+
+    def __repr__(self):
+        return str(self)
+
+person = Person('Dhruv', 24)
+with open('person.yml', 'w') as output_file:
+    yaml.dump(person, output_file)
